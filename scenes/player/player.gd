596919b387c03a2b8b72ef6fd576107455d0c7f5
Dr4gonMarine <65034442+Dr4gonMarine@users.noreply.github.com>
2026-08-03 15:27:17 -0300

    feat: Small attack animation and added IDE focused pluggins

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,18 +4,27 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const MOUSE_SENSITIVITY = 0.003
 
+var initial_weapon = load("res://scenes/weapons/sword.tscn")
+var current_weapon : Weapon
+
 @onready var head: Node3D = $Head
+@onready var hand : Node3D = %Hand
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	_equip_weapon(initial_weapon) 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
 		head.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
 		head.rotation.x = clamp(head.rotation.x, -PI / 2, PI / 2)
+		
 	if event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	if event.is_action_pressed("attack"):
+		current_weapon.attack()
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -35,3 +44,8 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func _equip_weapon(weapon: PackedScene) -> void:
+	var weapon_instace = weapon.instantiate() as Weapon
+	hand.add_child(weapon_instace) 
+	current_weapon = weapon_instace
